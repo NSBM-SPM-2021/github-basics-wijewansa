d120b0c054f8356970e89247a09b994ffd04d936
G.K.P.Wijewansa <gkpwijewansa@students.nsbm.ac.lk>
2021-10-11 23:10:03 +0530

    Add files via upload

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/spmproj21007902'),
+      dir: require('path').join(__dirname, './coverage/TimeTableUI'),
       subdir: '.',
       reporters: [
         { type: 'html' },
